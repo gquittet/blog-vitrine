e62701fa691970fcb232d8e0a6ce7feb898eda7d
Guillaume Quittet <guillaume.quittet@gmail.com>
2020-10-11 16:56:45 +0200

    chore(backend): setup admin endpoint

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,16 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://blog-vitrine.herokuapp.com/",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '0f188e7db9964535e6f244afcc1d690a'),
+      secret: env("ADMIN_JWT_SECRET", "0f188e7db9964535e6f244afcc1d690a"),
     },
+  },
+  proxy: {
+    enabled: true,
+    ssl: true,
+    host: "https://blog-vitrine.herokuapp.com/",
+    port: 443,
   },
 });
